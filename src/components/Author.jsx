0abe2c2e162a01e3b22c3086f98d20711d8dd169
ahad <ahadg446@gmail.com>
2024-08-10 00:45:58 +0500

    Updated Author for hover effect

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -3,15 +3,18 @@ import Logo from "../assets/Logo.jpg";
 
 const Author = () => {
   return (
-    <div className="fixed shadow-custom bottom-2 font-semibold right-2 md:bottom-1.5 md:right-1.5 bg-[--author-bg] text-[--author-text-color] text-xs p-2 rounded-lg z-10 flex items-center border-2 border-[--author-text-color] transition-colors duration-300">
+    <div className="fixed bottom-2 right-2 md:bottom-1.5 md:right-1.5 z-10">
       <a
         href="https://ahad324.github.io/AllProjects/"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center"
+        className="flex items-center bg-[--author-bg] text-[--author-text-color] text-xs p-2 rounded-lg border-2 border-[--author-text-color] transition-all duration-300 ease-in-out overflow-hidden whitespace-nowrap"
+        style={{ width: "3rem" }}
+        onMouseEnter={(e) => (e.currentTarget.style.width = "12rem")}
+        onMouseLeave={(e) => (e.currentTarget.style.width = "3rem")}
       >
-        <img src={Logo} alt="AbdulAhad" className="w-5 h-5 rounded-full" />
-        <span className="hidden sm:inline-block ml-2 underline cursor-pointer hover:text-gray-400">
+        <img src={Logo} alt="AbdulAhad" className="w-7 h-7 rounded-full" />
+        <span className="ml-3 hidden sm:inline-block hover:underline">
           Made by AbdulAhad
         </span>
       </a>
